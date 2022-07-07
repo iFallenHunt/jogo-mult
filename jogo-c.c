a334618c8b28d4f0af229d74b942ba02b1f72183
iFallenHunt <lukas_santos.silva@hotmail.com>
2022-07-07 17:45:06 -0300

    Um jogo onde é apresentado alguns valores e multiplicação

--- jogo-c.c
+++ jogo-c.c
@@ -4,20 +4,27 @@
 int main () {
 	int numero, i;
 
-	printf("---------------------------------\n");
-	printf(" Bem-Vindo ao jogo da Matematica \n");
-	printf("---------------------------------\n");
-	printf("\n");
-	printf("------- Digite um Numero! -------\n");
-	scanf("%d", &numero);
-	printf("\n");
+	while (numero != 0){
+		do{
+			printf("---------------------------------\n");
+			printf(" Bem-Vindo ao jogo da Matematica \n");
+			printf("---------------------------------\n");
+			printf("\n");
+			printf("------- Digite um Numero! -------\n");
+			scanf("%d", &numero);
+			printf("\n");
 
-	printf("----- Tabuada do Numero: %d ------\n", numero);
-	for (i = 1; i < 11; i++) {
-		printf("           %d x %d e: %d\n", numero, i,i * numero);
-	}
-	printf("\n");
+			printf("----- Tabuada do Numero: %d ------\n", numero);
+			for (i = 1; i < 11; i++) {
+				printf("           %d x %d e: %d\n", numero, i,i * numero);
+		}
+		printf("\n");
 	
-	printf("---- O Antecessor de: %d eh: %d ----\n", numero-1 );
-	printf("----- O Sucessor de: %d eh: %d -----\n", numero+1 );
+		printf("---- O Antecessor de: %d eh: %d ----\n", numero-1 );
+		printf("----- O Sucessor de: %d eh: %d -----\n", numero+1 );
+		}
+	while (numero!=0);
+			printf("\n------- Digite um Numero! -------\n");
+			scanf("%d", &numero);
+	}
 }
